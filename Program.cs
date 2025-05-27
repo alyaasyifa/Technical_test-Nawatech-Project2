cf2cd8a763f987960d607ec1af93ca88941fc7ec
alyaasyifa <alyasyf448@gmail.com>
2025-05-27 23:07:31 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace NawatechTest2
+{
+    using System;
+
+    namespace Project2_TargetFinder
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int[] numbers = { 1, 2, 3, 4, 5 };
+                int target = 4;
+
+                bool found = false;
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] == target)
+                    {
+                        Console.WriteLine($"Target has been reached in index number {i}");
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    Console.WriteLine("Target not found in array");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NawatechTest2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NawatechTest2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f0996e8c-d949-4a81-a7f2-24b1dbaa4d32")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
